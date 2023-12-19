d35b4b937226f68724fd4c8ed78599fc69f7f9dd
Abhimanyu <abhimanyu4@gmail.com>
2023-12-19 14:31:50 -0800

    common file contains str function to convert num to string and getcwd to get default current dir

--- cpp/project/common.h
+++ cpp/project/common.h
@@ -1,13 +1,21 @@
 #pragma once
 #include <string>
 #include <map>
+#include <filesystem>
 
 extern std::string sim_error;
+
+template<class T> std::string str(const T& input) { return std::to_string(input); }
 
 template<class T>
 inline T rand(T min, T max)
 {
     return min + rand() % (max - min + 1);
+}
+
+inline std::string getcwd()
+{
+    return std::filesystem::current_path().u8string();
 }
 
 /* Inverse map with limited size. Starts discarding lower entries with full */
