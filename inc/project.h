ce83c2bff47b7d3e9a5443fafc55ec0c78c00b4d
Abhimanyu <abhimanyu4@gmail.com>
2024-12-05 22:20:22 -0500

    Changed logging slightly in project.h
    Also notice the potential undefined state when returning the graphics state as a reference but also right after calling pop() on the queue.

--- inc/project.h
+++ inc/project.h
@@ -113,7 +113,7 @@ struct GraphicsHelper
 		}
 	}
 
-	std::thread gui_change_detect(Logger& logger, cow_v& cows)
+	std::thread gui_change_detect(cow_v& cows)
 	{
 		return std::thread([&]()
 			{
@@ -128,7 +128,7 @@ struct GraphicsHelper
 
 					if (!is_rendering) break;
 
-					auto& state = synced_state.front();
+					auto state = synced_state.front();
 					if (state.tx_idx >= 0)
 					{
 						update_tx(cows[state.tx_idx], state.ant_dir, state.ant_angle, state.ant_power, state.row, state.col, state.location);
@@ -165,7 +165,7 @@ struct GraphicsHelper
 
 	}
 
-	void render(Logger& logger, cow_v& txlist,
+	void render(cow_v& txlist,
 		const placement_v& mobile_stations_loc,
 		const placement_v& base_stations_loc,
 		const double_v& bs_txpower,
@@ -275,8 +275,7 @@ struct GraphicsHelper
 			is_rendering);
 	}
 
-	void plot(Logger& logger,
-		cow_v& txlist,
+	void plot(cow_v& txlist,
 		const placement_v& mobile_stations_loc,
 		const placement_v& base_stations_loc,
 		const double_v& bs_txpower,
@@ -329,7 +328,8 @@ struct GraphicsHelper
 		synced_state(num_transmitters),
 		is_rendering(true),
 		raw_cow_data(num_tx + 1),
-		raw_mrg_data(raw_cow_data.back())
+		raw_mrg_data(raw_cow_data.back()),
+		logger(ilogger)
 	{
 	}
 };
@@ -598,9 +598,9 @@ public:
 		simhelper->get_lut_tx_power(antenna_power);
 		simhelper->get_lut_scan_angle(scan_angles);
 
-		auto queue_thread = visuals.gui_change_detect(logger, cows);
+		auto queue_thread = visuals.gui_change_detect(cows);
 
-		visuals.render(logger, cows, mobile_stations_loc, base_stations_loc, antenna_power, bs_theta_c, scan_angles);
+		visuals.render(cows, mobile_stations_loc, base_stations_loc, antenna_power, bs_theta_c, scan_angles);
 
 		queue_thread.join();
 #endif
@@ -615,7 +615,7 @@ public:
 		simhelper->get_lut_tx_power(antenna_power);
 		simhelper->get_lut_scan_angle(scan_angles);
 
-		visuals.plot(logger, cows, mobile_stations_loc, base_stations_loc, antenna_power, bs_theta_c, scan_angles);
+		visuals.plot(cows, mobile_stations_loc, base_stations_loc, antenna_power, bs_theta_c, scan_angles);
 #endif
 	}
 
@@ -648,7 +648,7 @@ public:
 		scan_angle_range(args.scan_angle_range),
 		antenna_spacing(args.antenna_spacing),
 		antenna_dims(args.antenna_dims),
-		visuals(args.base_station_count, args.mobile_station_count, args.field_size[0], args.field_size[1]),
+		visuals(args.base_station_count, args.mobile_station_count, args.field_size[0], args.field_size[1], ilogger),
 		bs_tx_requested_power_watts(args.tx_powerlist().data),
 		bs_requested_scan_alpha_rad(args.tx_alphalist().data),
 		ms2bs_requested_bindings(args.ms_id_selections.binding_data)
