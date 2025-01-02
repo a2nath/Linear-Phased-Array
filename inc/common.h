8a24e7618067c8a9fa2eb56783b2549982c4cf34
Abhimanyu <abhimanyu4@gmail.com>
2025-01-02 18:47:11 -0500

    Add timing benchmark code in common.h

--- inc/common.h
+++ inc/common.h
@@ -75,6 +75,38 @@ inline std::string timestamp() {
 
 	return tstamp.str();
 }
+struct Bench
+{
+	std::chrono::steady_clock::time_point begin;
+	std::chrono::steady_clock::time_point end;
+	std::chrono::steady_clock::time_point mark_time;
+	long long delta;
+
+	inline auto get()
+	{
+		return delta;
+	}
+
+	inline void bench_start()
+	{
+		begin = std::chrono::steady_clock::now();
+		mark_time = begin;
+	}
+
+	inline void mark()
+	{
+		delta = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - mark_time).count();
+		mark_time = std::chrono::steady_clock::now();
+	}
+
+	inline void bench_stop()
+	{
+		end = std::chrono::steady_clock::now();
+		delta = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
+	}
+};
+
+
 
 template<class T>
 inline T rand(T min, T max)
