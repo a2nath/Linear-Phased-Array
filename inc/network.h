c498803adab3276a74e78609334e1995f1df0535
Abhimanyu <abhimanyu4@gmail.com>
2025-01-03 01:22:27 -0500

    move function def into antenna.cpp

--- inc/network.h
+++ inc/network.h
@@ -166,16 +166,10 @@ namespace network_package
 		/* accepts a RX station ID with respect to THIS station
 		   and returns the coefficient
 		*/
-		const double& coeff(const unsigned& rx_sta) const
-		{
-			return simulation.hmatrix[rx_sta];
-		}
+		const double& coeff(const unsigned& rx_sta) const;
 
 		/* hatrix with respect to pixel index (flattened from 2D) */
-		const double& gcoeff(const unsigned& pixel_idx) const
-		{
-			return graphic.hmatrix[pixel_idx];
-		}
+		const double& gcoeff(const unsigned& pixel_idx) const;
 
 		/* set power in watts */
 		void set_power(const double& power_watts)
@@ -274,157 +268,22 @@ namespace network_package
 		}
 
 		/*update the antenna array from updated powerand scan angle */
-		inline void update(Calculations& calculations)
-		{
-			/* update the antenna gain Gtx */
-			spdlog::info("Antenna update");
-			indices_with_inf.clear();
-			indices_with_z.clear();
-
-			for (long long idx = 0; idx < calculations.phee_minus_alpha_list.size(); ++idx)
-			{
-				if (calculations.pathloss_list[idx] == 0)
-				{ // this is going to be a inf
-					indices_with_inf.emplace_back(idx);
-					continue;
-				}
-				else if (calculations.gain_RX_grid[idx] == 0)
-				{ // this is going to be a zero
-					indices_with_z.emplace_back(idx);
-					continue;
-				}
-
-				double phee = (calculations.phee_minus_alpha_list[idx] + current.alpha) / 2;
-
-				double sin_term = current.panel_count * sin(phee);
-				double gain_factor_antenna_system = calculations.gain_RX_grid[idx]; // xN antennas already
-
-				if (sin_term != 0)
-				{
-					gain_factor_antenna_system *= cached::pow_2(cached::sin(current.panel_count * phee) / sin_term);
-				}
-
-				/* update the channel matrix */
-				calculations.hmatrix[idx] = gain_factor_antenna_system / calculations.pathloss_list[idx];
-			}
-
-			for (long i = 0; i < indices_with_inf.size(); ++i)
-			{
-				auto& problem_index = indices_with_inf[i];
-
-				if (0 <= problem_index - 1)
-				{
-					calculations.hmatrix[problem_index] = calculations.hmatrix[problem_index - 1];
-				}
-				else if (problem_index + 1 < calculations.hmatrix.size())
-				{
-					calculations.hmatrix[problem_index] = calculations.hmatrix[problem_index + 1];
-				}
-				// else all of them are infinity
-			}
-
-			for (long i = 0; i < indices_with_z.size(); ++i)
-			{
-				auto& problem_index = indices_with_z[i];
-
-				if (0 <= problem_index - 1)
-				{
-					calculations.hmatrix[problem_index] = calculations.hmatrix[problem_index - 1];
-				}
-				else if (problem_index + 1 < calculations.hmatrix.size())
-				{
-					calculations.hmatrix[problem_index] = calculations.hmatrix[problem_index + 1];
-				}
-			}
-		}
+		inline void update(Calculations& calculations);
 
 		/* for bare-minimum numerical calculations needed at the mobile_stations only */
-		void graphics_update()
-		{
-			if (graphic.modified)
-			{
-				update(graphic);
-				graphic.modified = false;
-			}
-		}
+		void graphics_update();
 
 		/* for bare-minimum numerical calculations needed at the mobile_stations only */
-		void numerical_update()
-		{
-			if (simulation.modified)
-			{
-				update(simulation);
-				simulation.modified = false;
-			}
-		}
+		void numerical_update();
 
 		/* re-calc the signal outs to handsets only (before calling update!) */
-		inline void init(const std::vector<Polar_Coordinates>& polar_data, Calculations& calculations)
-		{
-			const double& pioverlambda = M_PIl / current.lambda;
-			const double& phee_temp = 2 * current.spacing * pioverlambda;
-			const double& pl_temp_meters = 4 * pioverlambda;
-			const double& antenna_dim_factor = 10 * current.antenna_dims.x * current.antenna_dims.y / cached::pow_2(current.lambda);
-			double m_factor = current.antenna_dims.x * pioverlambda;
-
-			for (size_t idx = 0; idx < polar_data.size(); ++idx)
-			{
-				auto& cell_polar_data = polar_data[idx];
-
-				double theta_minus_thetaC = cell_polar_data.theta - current.theta_c;
-				double m = m_factor * cached::sin(theta_minus_thetaC);
-				double singleant_gain = antenna_dim_factor * cached::pow_2((1 + cached::cos(theta_minus_thetaC)) / 2);
-
-				if (m != 0)
-				{
-					singleant_gain *= cached::pow_2(cached::sin(m) / m);
-				}
-
-				calculations.phee_minus_alpha_list[idx] = phee_temp * cached::sin(theta_minus_thetaC);
-				calculations.pathloss_list[idx] = cached::pow_2(pl_temp_meters * cell_polar_data.hype);
-				calculations.gain_RX_grid[idx] = singleant_gain * current.panel_count;
-
-				if (calculations.pathloss_list[idx] == 0)
-				{
-					spdlog::warn("[index:" + str(idx) + "] " + str(calculations.pathloss_list[idx]) + " pathloss_list value is 0. " + \
-						"Possible reason, cell_polar_data {theta,hype}:{" + str(cell_polar_data.theta) + "," + str(cell_polar_data.hype) + "} hype is zero");
-
-				}
-				else if (std::isinf(calculations.pathloss_list[idx]))
-				{
-					spdlog::warn("[index:" + str(idx) + "] " + str(calculations.pathloss_list[idx]) + " pathloss_list value is inf. ");
-				}
-
-				if (calculations.gain_RX_grid[idx] == 0)
-				{
-					spdlog::warn("[index:" + str(idx) + "] " + str(calculations.gain_RX_grid[idx]) + " gain_RX_grid value is 0. " + \
-						"Possible reason, cos(theta_minus_thetaC):" + str(cos(theta_minus_thetaC)) + " + 1 = zero");
-
-				}
-				else if (std::isinf(calculations.gain_RX_grid[idx]))
-				{
-					spdlog::warn("[index:" + str(idx) + "] " + str(calculations.gain_RX_grid[idx]) + " gain_RX_grid value is inf. ");// +\
-						//"Possible reasons cos(theta_minus_thetaC):" + str(cos(theta_minus_thetaC)) + " + 1 = zero");
-				}
-			}
-		}
+		inline void init(const std::vector<Polar_Coordinates>& polar_data, Calculations& calculations);
 
 		/* for GUI simulation in the whole grid */
-		void graphics_init(const std::vector<Polar_Coordinates>& polar_data)
-		{
-			graphic.resize(polar_data.size());
-			init(polar_data, graphic);
-			graphic.modified = true;
-		}
+		void graphics_init(const std::vector<Polar_Coordinates>& polar_data);
 
 		/* for bare-minimum numerical calculations needed at the mobile_stations only */
-		void numerical_init(const std::vector<Polar_Coordinates>& polar_data)
-		{
-			simulation.resize(polar_data.size());
-			init(polar_data, simulation);
-			simulation.modified = true;
-		}
-
+		void numerical_init(const std::vector<Polar_Coordinates>& polar_data);
 
 		/* change the state back to the previous one */
 		void undo()
